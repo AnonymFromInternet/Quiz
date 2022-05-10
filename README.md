2bb5fda4bd7929cee60df6156247dea30cb5512e
AnonymFromInternet <84092079+AnonymFromInternet@users.noreply.github.com>
2022-05-10 04:36:18 +0200

    Adding Components and DB

---
+++ README.md
@@ -0,0 +1,4 @@
+# This is Quiz project
+### Uses the following packages:
+
+
--- src/data.js
+++ src/data.js
@@ -1,42 +1,66 @@
 const data = [
   {
-    question: "Question",
+    question: "What does CSS stand for?",
     incorrectAnswers: [
-      "Incorrect answer",
-      "Incorrect answerA",
-      "Incorrect answerC",
-      "Incorrect answerE",
+      "Computer Style Sheets",
+      "Creative Style Sheets",
+      "Colorful Style Sheets",
     ],
-    correctAnswer: "Correct answer",
+    correctAnswer: "Cascading Style Sheets",
+  },
+
+  {
+    question:
+      "Where in an HTML document is the correct place to refer to an external style sheet?",
+    incorrectAnswers: [
+      "In the <body> section",
+      "At the end of the document",
+      "You can't refer to an external style sheet",
+    ],
+    correctAnswer: "In the <head> section",
   },
   {
-    question: "QuestionA",
-    incorrectAnswers: [
-      "Incorrect answer",
-      "Incorrect answerA",
-      "Incorrect answerC",
-      "Incorrect answerE",
-    ],
-    correctAnswer: "Correct answer",
+    question: "Which HTML tag is used to define an internal style sheet?",
+    incorrectAnswers: ["<script>", "<headStyle>", "<css>"],
+    correctAnswer: "<style>",
   },
   {
-    question: "QuestionC",
-    incorrectAnswers: [
-      "Incorrect answer",
-      "Incorrect answerA",
-      "Incorrect answerC",
-      "Incorrect answerE",
-    ],
-    correctAnswer: "Correct answer",
+    question: "Which HTML attribute is used to define inline styles?",
+    incorrectAnswers: ["class", "font", "styles"],
+    correctAnswer: "style",
   },
   {
-    question: "QuestionE",
+    question: "Which is the correct CSS syntax?",
     incorrectAnswers: [
-      "Incorrect answer",
-      "Incorrect answerA",
-      "Incorrect answerC",
-      "Incorrect answerE",
+      "{body:color=black;}",
+      "{body;color:black;}",
+      "body:color=black;",
     ],
-    correctAnswer: "Correct answer",
+    correctAnswer: "body {color: black;}",
+  },
+  {
+    question: "How do you insert a comment in a CSS file?",
+    incorrectAnswers: [
+      "' this is a comment",
+      "// this is a comment",
+      "// this is a comment //",
+    ],
+    correctAnswer: "/* this is a comment */",
+  },
+  {
+    question: "Which property is used to change the background color?",
+    incorrectAnswers: ["color", "bgcolor", "bgColor"],
+    correctAnswer: "background-color",
+  },
+  {
+    question: "How do you add a background color for all <h1> elements?",
+    incorrectAnswers: [
+      "all.h1 {background-color:#FFFFFF;}",
+      "h1.setAll {background-color:#FFFFFF;}",
+      "h1.all {background-color:#FFFFFF;}",
+    ],
+    correctAnswer: "h1 {background-color:#FFFFFF;}",
   },
 ];
+
+export default data;
